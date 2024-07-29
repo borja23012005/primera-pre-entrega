eda419b1b78bbd2cedf25cf29a79a848c979d39e
Borja Chavarria <borjachavarria2005@gmail.com>
2024-07-29 20:21:51 -0300

    primera-preentrega

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+// function enviarMensaje (){
+//     console.log("mostrando mensaje");
+// }
+// enviarMensaje ()
+// enviarMensaje ()
+// enviarMensaje ()
+// enviarMensaje ()
+function enviarMensaje (){
+    let ingreseSuNombre = prompt("ingrese su nombre");
+    console.log("el nombre ingresado es:" + ingreseSuNombre)
+}
+for(let i =0; i < 5; i++){
+    ingresarNombre()
+}
